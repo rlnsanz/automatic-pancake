24e2a358b4d0b86a5811f35ea3370993a62f26e1
Rolando Garcia <rogarcia@berkeley.edu>
2023-11-16 20:00:24 -0600

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
-from flask import Flask
-from .routes import setup_routes
+from flask import Flask, render_template
 
-def create_app():
-    app = Flask(__name__)
-    setup_routes(app)
-    return app
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
